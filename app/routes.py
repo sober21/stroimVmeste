40c861d57f61b1bad6ce7ecdcd21981edfa7cbdd
Trezvy_Papa <mix_mmg@mail.ru>
2024-05-03 13:33:58 +0300

    Добавлены html-страницы и одноименные функции: index, price, about_us и шаблон base.html

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,17 @@
+from flask import render_template, url_for
 from app import app
+
 
 @app.route('/')
 def index():
-    return '89204247011'
+    return render_template('index.html', title='Главная')
+
+
+@app.route('/about_us')
+def about_us():
+    return render_template('about_us.html', title='О нас')
+
+
+@app.route('/price')
+def price():
+    return render_template('price.html', title='Цены')
