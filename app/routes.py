1cb172867914e4de53d0920a4f5274b6fdef239d
trezvypapa <mix_mmg@mail.ru>
2024-06-13 12:02:18 +0300

    Первая версия. Одностраничная

--- app/routes.py
+++ app/routes.py
@@ -4,14 +4,5 @@ from app import app
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='Главная')
+    return render_template('index.html')
 
-
-@app.route('/about_us')
-def about_us():
-    return render_template('about_us.html', title='О нас')
-
-
-@app.route('/price')
-def price():
-    return render_template('price.html', title='Цены')
