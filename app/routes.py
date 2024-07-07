7f5602591c70d37f41de84a82286b3a44a6e72e4
trezvypapa <mix_mmg@mail.ru>
2024-07-07 22:22:51 +0300

    Добавил страницу myagkaya_krovlya. Добавил в подвал карту Яндекс, навигацию по сайту и контакты для связи.

--- app/routes.py
+++ app/routes.py
@@ -17,6 +17,11 @@ def naplav():
     return render_template('naplavlyaemaya.html')
 
 
+@app.route('/myagkaya_krovlya')
+def myagkaya():
+    return render_template('myagkaya.html')
+
+
 @app.route('/price')
 def price():
     return render_template('price.html')
